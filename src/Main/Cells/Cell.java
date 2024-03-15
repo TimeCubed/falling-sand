cf0deaa9a8474ae05d510800452b8cb6bf74bb97
SummerNugget <summernugget1@gmail.com>
2024-03-16 01:23:16 +0200

    Added flag for checking if a cell was updated in the last render tick or not

--- src/Main/Cells/Cell.java
+++ src/Main/Cells/Cell.java
@@ -4,13 +4,32 @@ import Main.PixelDrawer;
 
 public abstract class Cell {
 	protected int x, y;
+	protected boolean updated;
 	protected PixelDrawer pixelDrawer;
 	
 	public Cell(PixelDrawer pixelDrawer, int x, int y) {
 		this.x = x;
 		this.y = y;
+		
+		this.pixelDrawer = pixelDrawer;
 	}
 	
-	public abstract void update();
+	/**
+	 * Call to update this cell's position and relevant state information.
+	 *
+	 * @return A 2 element integer array containing the new X and Y positions after updating
+	 */
+	public abstract int[] update();
+	
+	/**
+	 * Draws the current cell using the internal <code>PixelDrawer</code> object from the constructor.
+	 */
 	public abstract void draw();
+	
+	public void setUpdated(boolean updated) {
+		this.updated = updated;
+	}
+	public boolean hasUpdated() {
+		return this.updated;
+	}
 }
