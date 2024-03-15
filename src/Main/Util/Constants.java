4870f393e63569697afca39f1dd03c1c6abd9bc3
SummerNugget <summernugget1@gmail.com>
2024-03-16 01:27:36 +0200

    Changed SCREEN_WIDTH and SCREEN_HEIGHT parameters to a nicer size

--- src/Main/Util/Constants.java
+++ src/Main/Util/Constants.java
@@ -1,7 +1,7 @@
 package Main.Util;
 
 public class Constants {
-	public static final int SCREEN_WIDTH = 500;
-	public static final int SCREEN_HEIGHT = 500;
+	public static final int SCREEN_WIDTH = 100;
+	public static final int SCREEN_HEIGHT = 150;
 	public static final int PIXEL_SIZE = 3;
 }
