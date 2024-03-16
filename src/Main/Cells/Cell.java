c60ba0d7bbc5962454541c1169984e5475428ec8
SummerNugget <summernugget1@gmail.com>
2024-03-16 03:29:09 +0200

    Added createCluster method for future use

--- src/Main/Cells/Cell.java
+++ src/Main/Cells/Cell.java
@@ -47,6 +47,7 @@ public abstract class Cell {
 	/**
 	 * Call to update this cell's position and relevant state information.
 	 *
+	 * @param board The game board with all the cells in it for neighbor data
 	 * @return A 2 element integer array containing the new X and Y positions after updating
 	 */
 	public abstract int[] update(final Cell[][] board);
@@ -55,6 +56,15 @@ public abstract class Cell {
 	 * Draws the current cell using the internal <code>PixelDrawer</code> object from the constructor.
 	 */
 	public abstract void draw();
+	
+	/**
+	 * Creates a cluster of this cell at the given location
+	 *
+	 * @param x The X position to create the cluster of cells at
+	 * @param y The Y position to create the cluster of cells at
+	 * @param board The game board used for neighbor data
+	 */
+	public abstract void createCluster(int x, int y, final Cell[][] board);
 	
 	public void setUpdated(boolean updated) {
 		this.updated = updated;
