057959ff0b92c5c6d0b8821f7d52ee64961ee882
SummerNugget <summernugget1@gmail.com>
2024-03-17 01:34:05 +0200

    Rewrote update() and added code for sand mound formation

--- src/Main/Cells/SandCell.java
+++ src/Main/Cells/SandCell.java
@@ -5,23 +5,85 @@ import Main.Util.Constants;
 import java.awt.*;
 
 public class SandCell extends Cell {
+	private final int maxTravel;
+	
 	public SandCell(PixelDrawer pixelDrawer, int x, int y) {
 		super(pixelDrawer, x, y);
 		
 		int brightnessOffset = this.random.nextInt(41) - 20;
 		
 		super.setColor(new Color(194 + brightnessOffset, 178 + brightnessOffset, 128 + brightnessOffset));
+		
+		switch (random.nextInt(3) - 1) {
+			case -1 -> horizontalTravelDirection = HorizontalTravelDirections.LEFT;
+			case 0 -> horizontalTravelDirection = HorizontalTravelDirections.NO_TRAVEL;
+			case 1 -> horizontalTravelDirection = HorizontalTravelDirections.RIGHT;
+		}
+		
+		maxTravel = random.nextInt(3, 6);
 	}
 	
 	@Override
 	public int[] update(final Cell[][] board) {
-		this.y += 1;
+		this.prevX = this.x;
+		this.prevY = this.y;
 		
-		if (this.y >= Constants.SCREEN_HEIGHT || board[this.x][this.y] != null) {
-			this.y -= 1;
+		validatePosition();
+		
+		if (!groundCheck() && board[this.x][this.y + 1] == null) {
+			this.y += 1;
+		} else if (!leftEdgeCheck() && !groundCheck() && board[this.x - 1][this.y + 1] == null) {
+			this.x -= 1;
+			this.y += 1;
+		} else if (!rightEdgeCheck() && !groundCheck() && board[this.x +1][this.y + 1] == null) {
+			this.x += 1;
+			this.y += 1;
 		}
 		
+		validatePosition();
+		
+		if (random.nextInt(2) == 0 && totalHorizontalTravel < maxTravel && !horizontalEdgeCheck() && board[this.x + horizontalTravelDirection][this.y] == null) {
+			this.x += horizontalTravelDirection;
+			totalHorizontalTravel++;
+		}
+		
+		validatePosition();
+		
 		return new int[] {this.x, this.y};
+	}
+	
+	public void validatePosition() {
+		if (this.x >= Constants.SCREEN_WIDTH) {
+			this.x = Constants.SCREEN_WIDTH - 1;
+		}
+		
+		if (this.x < 0) {
+			this.x = 0;
+		}
+		
+		if (this.y >= Constants.SCREEN_HEIGHT) {
+			this.y = Constants.SCREEN_HEIGHT - 1;
+		}
+		
+		if (this.y < 0) {
+			this.y = 0;
+		}
+	}
+	
+	public boolean leftEdgeCheck() {
+		return this.x == 0;
+	}
+	
+	public boolean rightEdgeCheck() {
+		return this.x == Constants.SCREEN_WIDTH - 1;
+	}
+	
+	public boolean horizontalEdgeCheck() {
+		return leftEdgeCheck() || rightEdgeCheck();
+	}
+	
+	public boolean groundCheck() {
+		return this.y == Constants.SCREEN_HEIGHT - 1;
 	}
 	
 	@Override
